788c2a218f361900831d3b782fab8c394ca644bc
PedroVine345 <pn260798@gmail.com>
2024-11-13 15:30:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Brawstars',
+    'Quais são os melhores modos de jogo para subir trofeu?',
+    'O combate solitario e Fut'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
